87307d6a869eb3e552459248684ac37936e2b0b2
4iklins <4iklins@gmail.com>
2023-08-18 17:09:46 +0300

    file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './layout/Header/Header';
+import Main from './layout/Main/Main';
+import Footer from './layout/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className='App'>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
